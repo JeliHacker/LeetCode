e270f7d07d7b1d12b48c6f8af7d3dad8a54d3675
JeliHacker <jeligooch@gmail.com>
2023-02-25 14:03:45 -0500

    2567. Minimum Score by Changing Two Elements

---
+++ 2567_minimum_score_by_changing_two_elements.py
@@ -0,0 +1,12 @@
+# https://leetcode.com/problems/minimum-score-by-changing-two-elements/description/
+
+class Solution:
+    def minimizeSum(self, nums: List[int]) -> int:
+        n = len(nums)
+        nums.sort()
+        return min(
+            abs(nums[n - 3] - nums[0]),
+            abs(nums[n - 2] - nums[1]),
+            abs(nums[n - 1] - nums[2])
+        )
+    
