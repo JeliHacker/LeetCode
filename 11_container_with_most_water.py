7bd484af73c8ee10cd39149e94c883d63e2bb7ed
JeliHacker <jeligooch@gmail.com>
2023-02-26 21:41:34 -0500

    11. Container with most water, original solution

--- 11_container_with_most_water.py
+++ 11_container_with_most_water.py
@@ -2,30 +2,16 @@
 
 class Solution:
     def maxArea(self, height: List[int]) -> int:
-        # Brute Force solution:
-        # answer = 0
-        
-        # for left in range(len(height)):
-        #     for right in range(left + 1, len(height)):
-        #         area = (right - left) * min(height[left], height[right])
-        #         answer = max(answer, area)
-
-        # return answer
-
-
-        # O(n) solution
-        answer = 0
         left = 0
         right = len(height) - 1
+        answer = 0
 
         while left < right:
-            area = (right - left) * min(height[left], height[right])
+            area = min(height[left], height[right]) * (right - left)
             answer = max(answer, area)
-            smallerHeight = 0
-            if height[left] <= height[right]:
-                left += 1
-            else:
+            if height[left] > height[right]:
                 right -= 1
-
+            else:
+                left += 1
+            
         return answer
-
